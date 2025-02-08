90c2e0753e709fdf1761d6ec134815840111b058
hxulin <1455936807@qq.com>
2025-02-08 11:14:46 +0800

    安装脚本完善

--- install.sh
+++ install.sh
@@ -16,11 +16,10 @@ config_after_install() {
         echo -e "${yellow}您的账户密码将设定为:${config_password}${plain}"
         read -p "请设置面板访问端口:" config_port
         echo -e "${yellow}您的面板访问端口将设定为:${config_port}${plain}"
-        echo -e "${yellow}确认设定,设定中${plain}"
         ~/x-ui/x-ui setting -username ${config_account} -password ${config_password}
         echo -e "${yellow}账户密码设定完成${plain}"
         ~/x-ui/x-ui setting -port ${config_port}
-        echo -e "${yellow}面板端口设定完成${plain}"
+        echo -e "\n${yellow}面板端口设定完成${plain}"
     else
         echo -e "${red}已取消,所有设置项均为默认设置,请及时修改${plain}"
     fi
